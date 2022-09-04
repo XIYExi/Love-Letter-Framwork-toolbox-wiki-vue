003047bcf5e8ff587ac675061a282643e245da1d
xiye <Zhou730428@outlook.com>
2022-09-04 08:35:39 +0800

    完成基本布局

---
+++ src/components/wiki-footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Love Letter Framework toolbox wiki
+  </a-layout-footer>
+</template>
+
+<script>
+export default {
+  name: "wiki-footer"
+}
+</script>
+
+<style scoped>
+
+</style>
